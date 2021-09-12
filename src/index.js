31dad45cdebccb09cc97b7a357bc8f71f54e2fa8
shamill72 <shane.d.hamill@gmail.com>
2021-09-12 12:44:13 -0500

    changed baseUrl to basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter baseUrl="/">
+  <BrowserRouter basename="/crwn-clothing">
     <App />
     </BrowserRouter>,
     document.getElementById('root')
