ce5795c99595c2f26d2d4ad25d55591fcaaa27cc
shamill72 <shane.d.hamill@gmail.com>
2021-09-12 12:35:47 -0500

    added BaseUrl back

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter baseUrl="/crwn-clothing/">
     <App />
     </BrowserRouter>,
     document.getElementById('root')
